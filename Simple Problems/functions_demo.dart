ff85df6cd8b65c9d68e2d17da7f40b6bd685b826
Mubin <fathummubin26@gmail.com>
2026-01-20 23:09:09 +0600

    Update backend and frontend files

--- Simple Problems/functions_demo.dart
+++ Simple Problems/functions_demo.dart
@@ -4,9 +4,7 @@ void main() {
   int sum = add(10, 20);
   print('Sum: $sum');
 
-  describePerson(name: 'Alice', age: 30);
-  
-  print('Square of 5: ${calculateSquare(5)}');
+  describePerson(name: 'Alice', 
 }
 
 // Basic function
